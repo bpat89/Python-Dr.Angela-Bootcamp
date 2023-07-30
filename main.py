f7ffbf6fb4e83abf71aad155e0e61443d3f807df
Patrick <bilshanpatrick@gmail.com>
2023-07-30 11:24:06 +0400

    Using the for loop with Python Lists_Average Heights

--- main.py
+++ main.py
@@ -1,55 +1,26 @@
-import random
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
 
-rock = '''
-    _______
----'   ____)
-      (_____)
-      (_____)
-      (____)
----.__(___)
-'''
 
-paper = '''
-    _______
----'   ____)____
-          ______)
-          _______)
-         _______)
----.__________)
-'''
+#Write your code below this row 👇
+# 156 178 165 171 187 156 178
+# ask the heights of 7 people
+# add all of them
+# divide the result by 7
+# round the answer to a whole number and print the result
+total_sum = 0
+total_students = 0
+for student_height in student_heights:
+    total_sum += int(student_height)
+for amount in student_heights:
+    total_students += 1
+print(total_sum)
+print(total_students)
+average = round(total_sum/total_students)
+print(average)
 
-scissors = '''
-    _______
----'   ____)____
-          ______)
-       __________)
-      (____)
----.__(___)
-'''
 
-icons = [rock, paper, scissors]
-player = int(input("choose your weapon: 1 for Rock, 2 for paper, 3 for scissors : \n"))
-computer = random.randint(0,3)
-player_image = icons[player-1]
-computer_image = icons[computer-1]
-if player == computer:
-    print(f"you chose {player} \n {player_image}")
-    print(f"the computer chose {computer}\n {computer_image}")
-    print("its a draw")
-elif player == 1 and computer == 3:
-    print(f"you chose {player}\n {player_image}")
-    print(f"the computer chose {computer}\n {computer_image}")
-    print("you win")
-elif player == 3 and computer == 2:
-    print(f"you chose {player}\n {player_image}")
-    print(f"the computer chose {computer}\n {computer_image}")
-    print("you win")
-elif player == 2 and computer == 1:
-    print(f"you chose {player}\n {player_image}")
-    print(f"the computer chose {computer}\n {computer_image}")
-    print("you win")
-else:
-    print(f"you chose {player}\n {player_image}")
-    print(f"the computer chose {computer}\n {computer_image}")
-    print("you loose")
 
