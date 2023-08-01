802ac004c75742b896cae5c09be682132a571c19
Patrick <bilshanpatrick@gmail.com>
2023-08-01 13:58:28 +0400

    [Interactive Coding Exercise] Adding Even Numbers

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-# for number in range(1, 11, 3):
-#     print(number)
-
 total = 0
 for numbers in range(1,101):
-    total += numbers
+    if numbers % 2 == 0:
+        total += numbers
 print(total)
