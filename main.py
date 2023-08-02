db630875d99e88f1db04ff2a95cba19f88bdc137
Patrick <bilshanpatrick@gmail.com>
2023-08-02 15:13:25 +0400

    Beginners_Hangman_Picking a Random Words and Checking Answers

--- main.py
+++ main.py
@@ -1,53 +1,12 @@
-#Password Generator Project
+#Step 1
 import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n"))
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+guess = input("Choose a letter :").lower()
 
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-
-nr_numbers = int(input(f"How many numbers would you like?\n"))
-
-#Eazy Level - Order not randomised:
-#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
-# password = ""
-# for char in range(1, nr_letters+1):
-#     password += random.choice(letters)
-# for symb in range(1,nr_symbols+1):
-#     password += random.choice(symbols)
-# for nums in range(1, nr_numbers+1):
-#     password += random.choice(numbers)
-# print(password)
-#Hard Level - Order of characters randomised:
-#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
-password = []
-for char in range(1, nr_letters+1):
-    password += random.choice(letters)
-for symb in range(1,nr_symbols+1):
-    password += random.choice(symbols)
-for nums in range(1, nr_numbers+1):
-    password += random.choice(numbers)
-    random.shuffle(password)
-random_password = ""
-for char in password:
-    random_password += char
-print(random_password)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for letter in chosen_word:
+    if letter == guess:
+        print("True")
+    else:
+        print("False")
