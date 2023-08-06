1ade4d4592b7d11c52c09851a25cf3f03f466af5
Patrick <bilshanpatrick@gmail.com>
2023-08-06 10:12:20 +0400

    Function Parameters & Ceasar Cipher_Positional vs Keyword Arguments

--- main.py
+++ main.py
@@ -8,9 +8,16 @@
 #     print("Today is a Clear day for victory")
 #     print("universe is in my favor all the time")
 # greet()
-newName = input("Please enter your name : ")
-def greet_with_name (name):
+# newName = input("Please enter your name : ")
+# def greet_with_name (name):
+#     print( f"Good Morning {name}")
+#     print(f"Today is a Clear day for victory")
+#     print(f"universe is in my favor all the time")
+# greet_with_name(newName)
+new_Name = input("Please enter your name : ")
+new_location = input("Please enter your location : ")
+def greet_with_name (name,location):
     print( f"Good Morning {name}")
     print(f"Today is a Clear day for victory")
-    print(f"universe is in my favor all the time")
-greet_with_name(newName)
+    print(f"Universe is in my favor all the time {location}")
+greet_with_name(location = new_location, name = new_Name)
