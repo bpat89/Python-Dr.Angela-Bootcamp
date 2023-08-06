11b314af280f696af9c49cead4b4f16f15a1e313
Patrick <bilshanpatrick@gmail.com>
2023-08-06 11:48:42 +0400

    Function Parameters & Ceasar Cipher_Prime Number Check

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
-#Write your code below this line 👇
-import math
+# Write your code below this line 👇
+def prime_checker(number):
+    prime_number = True
+    for i in range(2,number):
+        if number % i == 0:
+            prime_number = False
+    if prime_number:
+        print("It's a prime number.")
+    else:
+        print("It's not a prime number")
+# Write your code above this line 👆
 
-
-def paint_calc(height, width, cover):
-    area = height * width
-    num_of_cans = math.ceil(area / cover)
-    print(f"You'll need {num_of_cans} cans of paint.")
-
-
-
-
-
-
-#Write your code above this line 👆
-# Define a function called paint_calc() so that the code below works.
-
-# 🚨 Don't change the code below 👇
-test_h = int(input("Height of wall: "))
-test_w = int(input("Width of wall: "))
-coverage = 5
-paint_calc(height=test_h, width=test_w, cover=coverage)
-
+# Do NOT change any of the code below👇
+n = int(input("Check this number: "))
+prime_checker(number=n)
