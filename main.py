3608d261295724fb1771bd77bc956ab06261c726
Patrick <bilshanpatrick@gmail.com>
2023-08-09 16:21:46 +0400

    Dictionaries, Nesting and the Secrete Auction_Code Challenge_Dictionary in a List

--- main.py
+++ main.py
@@ -1,38 +1,26 @@
-###################################################################################################
-# Nesting
-capitals = {
-    "France" : "Paris",
-    "Germany" : "Berlin",
-}
-# Nesting a List in a Dictionary
-travel_log = {
-    "France": ["Paris", "Lille", "Dijon"],
-    "Germany": ["Berlin", "Frankfurt", "Hamburg"],
-}
-# print(travel_log)
-# Dictionary inside a Dictionary
-# travel_log = {
-#     "France": {"cities_visited": ["Paris", "Lille", "Dijon"]},
-#     "Germany": ["Berlin", "Frankfurt", "Hamburg"],
-# }
-# print(travel_log)
-# travel_log = {
-#     "France": {"cities_visited": ["Paris", "Lille", "Dijon"],"total_visits":12},
-#     "Germany": {"cities_visited": ["Berlin", "Frankfurt", "Hamburg"], "total_visits":20},
-# }
-# print(travel_log)
-# Nesting Dictionary in a List
 travel_log = [
-    {
-        "Country": "France",
-        "cities_visited": ["Paris","Lille","Dijon"],
-        "total_visits": 13
-    },
-    {
-        "Country": "Germany",
-        "cities_visited": ["Frankfurt","Berlin","Hamburg"],
-        "total_visits": 7,
-    }
-    ]
-print(travel_log)
+{
+  "country": "France",
+  "visits": 12,
+  "cities": ["Paris", "Lille", "Dijon"]
+},
+{
+  "country": "Germany",
+  "visits": 5,
+  "cities": ["Berlin", "Hamburg", "Stuttgart"]
+},
+]
+#🚨 Do NOT change the code above
 
+#TODO: Write the function that will allow new countries to be added to the travel_log. 👇
+
+def add_new_country(country,visits,cities):
+    new_country = {
+        "country": country,
+        "visits": visits,
+        "cities": cities
+    }
+    travel_log.append(new_country)
+#🚨 Do not change the code below
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+print(travel_log)
