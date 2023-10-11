c2d2034b7ed67d6bb084a228778ad4deb5510f1f
Patrick <bilshanpatrick@gmail.com>
2023-10-11 17:24:06 +0400

    Functions with Outputs_Doc_Strings

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
+def format_name(f_name, l_name):
+    """ Takes the first and last name and converts into title case version of the name"""
+    if f_name == "" or l_name == "":
+        return "You didn't provide a value"
+    format_fname = f_name.title()
+    format_lname = l_name.title()
+    return f"{format_fname} {format_lname}"
+
+print(format_name(input("First Name : "), input("Last Name: ")))
+
+"""" Now if you call the function format_name(),
+ as soon as you bring the arrow into the parenthesis you will see the description of the function""""
 
 
-def is_leap(year):
-    if year % 4 == 0:
-        if year % 100 == 0:
-            if year % 400 == 0:
-                return True
-            else:
-                return False
-        else:
-            return True
-    else:
-        return False
 
-def days_in_month(year, month):
-    month_days = [31,28,31,30,31,30,31,31,30,31,30,31]
-    if month == 2 and is_leap(year):
-        return 29
-    else:
-        return month_days[month-1]
-year = int(input())
-month = int(input())
-days = days_in_month(year, month)
-print(days)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
