a90bc215d07dc6b514ee1461d7360dba2b943c32
Patrick <bilshanpatrick@gmail.com>
2023-10-13 21:29:08 +0400

    Functions with Outputs_Calculator_While Loops, Flags and Recursion

--- main.py
+++ main.py
@@ -1,40 +1,44 @@
-# Calculator
+# calculator
+
+
+
 def add(n1, n2):
     return n1 + n2
-
 def subtract(n1, n2):
     return n1 - n2
-
 def multiply(n1, n2):
     return n1 * n2
-
 def divide(n1, n2):
     return n1 / n2
-# created dictionary and stored in the variable named operations
-operations = {
+
+operating_symbols = {
     "+": add,
     "-": subtract,
     "*": multiply,
     "/": divide
 }
-num1 = int(input("What's your first number? : \n"))
-for symbol in operations:
-    print(symbol)
-operating_symbol = input("Pick a symbol from the lines above \n")
-num2 = int(input("What's your second number? : \n"))
-# calling the function from the dictionary
-operation = operations[operating_symbol]
-# giving values for n1 and n2 to the callout function
-first_answer = operation(num1,num2)
-print(f"{num1} {operating_symbol} {num2} = {first_answer}")
-operating_symbol = input("Pick a symbol from the lines above \n")
-num3 = int(input("What's your second number? : \n"))
-operation = operations[operating_symbol]
-second_answer = operation(first_answer,num3)
+def calculator():
+    num1 = int(input("please enter you first number \n"))
+    for symbols in operating_symbols:
+        print(symbols)
+    to_continue = True
+    while to_continue:
+        operation = input("please select the operator \n ")
+        num2 = int(input("please enter the next number \n"))
+        function = operating_symbols[operation]
+        answer = function(num1,num2)
+        print(f"{num1} {operation} {num2} = {answer}")
+        if input("would you like to continue type 'y' for yes and 'n' to exit ") == "y":
+            num1 = answer
+        else:
+            to_continue = False
+            calculator()
 
-print(second_answer)
+calculator()
 
-print(f"{first_answer} {operating_symbol} {num3} = {second_answer}")
+
+
+
 
 
 
