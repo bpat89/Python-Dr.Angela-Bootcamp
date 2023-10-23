5b69bb2c640d2d816214f1940458659445fa5371
Patrick <bilshanpatrick@gmail.com>
2023-10-23 20:38:38 +0400

    Scope and Number Guessing Game-Namespaces: Practice_added a while loop and called the game function

--- main.py
+++ main.py
@@ -3,17 +3,20 @@ import random
 
 EASY_LEVEL = 10
 HARD_LEVEL = 5
+
 # Function to check user's answer against actual number.
-def check_answer(guess, answer, turns):
+def check_answer(guess, answer):
     if guess > answer:
         print("Too High")
     elif guess < answer:
         print("Too Low")
     else:
-        print(f"You got it! The answer was {answer}.")
+        print(f"You got it right, the correct answer is {answer} ")
+
+
 # Make function to set the difficulty.
 def set_difficulty():
-    level = input("Choose a difficulty. Type 'easy' or 'hard': ")
+    level = input("choose the level as easy or hard. ")
     if level == "easy":
         return EASY_LEVEL
     else:
@@ -22,19 +25,20 @@ def set_difficulty():
 def game():
     # Choose a Random Number between 1 and 100.
     print(art.logo)
-    print("Welcome to the Number Guessing Game!")
-    print("I'm thinking of a number between 1 and 100.")
-    answer = random.randint(0,100)
-    print(f"Pssst, the correct answer is {answer}")
-    turns = set_difficulty()
-    print(f"You have {turns} attempts remaining to guess the number.")
+    print("Welcome to the Mind Reader Numbers")
+    print("Im thinking of a number between 0 and 100 .")
+    answer = random.randint(0, 100)
+    print(f"Psst,  here's the hint {answer}")
+
+    # Let the user guess the number.
+    attempts = set_difficulty()
+    print(f"your remaining attempts are {attempts}")
     # Repeat the guessing if they get it wrong.
     guess = 0
     while guess != answer:
-        # Let the user guess the number.
-        guess = int(input("Make a guess: "))
+        guess = int(input("Make a Guess. : "))
         check_answer(guess, answer)
 
-# Track the number of turns and reduce it by 1 if they chose wrong.
-game()
 
+    # Track the number of turns and reduce it by 1 if they chose wrong.
+game()
