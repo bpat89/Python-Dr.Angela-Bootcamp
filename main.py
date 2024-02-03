d164d44a1f9b97b536b23626712eb102f1c2976e
Patrick <bilshanpatrick@gmail.com>
2024-02-03 14:48:49 +0400

    Intermediate - Object Oriented Programming OOP: Constructing Objects and Accessing their Attributes and Methods

--- main.py
+++ main.py
@@ -1,42 +1,13 @@
-import art
-import random
+from turtle import Turtle, Screen
 
-EASY_LEVEL = 10
-HARD_LEVEL = 5
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(100)
+my_screen = Screen()
 
-def check_answer(guess, answer, turns):
-    """ checks guess against answer and returns the turns remaining. """
-    if guess > answer:
-        print("Too High")
-        return turns -1
-    elif guess < answer:
-        print("Too Low")
-        return turns -1
-    else:
-        print(f"You've got it right ! {guess} is the correct answer. ")
+print(my_screen.canvheight)
+my_screen.exitonclick()
 
-def set_difficulty():
-    level = input("Choose your difficult level. Type 'easy' or 'hard' : \n")
-    if level == "easy":
-        return EASY_LEVEL
-    else:
-        return HARD_LEVEL
-def game():
-    print(art.logo)
-    print("Welcome to the Numbers Game.")
-    print("I'm guessing a number between 0 and 100. ")
-    answer = random.randint(0, 100)
-    print(f"The hint is {answer}")
-    turns = set_difficulty()
-    guess = 0
-    while guess != answer:
-        print(f"you have {turns} turns remaining.")
-        guess = int(input("Please make a guess. \n"))
-        turns = check_answer(guess, answer, turns)
-        if turns == 0:
-            print("you are out of turns. You Loose. ")
-            return
-        elif guess != answer:
-            print("Guess again. ")
 
-game()
