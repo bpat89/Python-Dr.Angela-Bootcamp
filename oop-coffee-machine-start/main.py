d83deae56cb43f0ceaf8622115d1a4498c07dab6
Patrick <bilshanpatrick@gmail.com>
2024-02-03 20:42:54 +0400

    Intermediate - Object Oriented Programming OOP: Building The Coffee Machine-Starting Code

---
+++ oop-coffee-machine-start/main.py
@@ -0,0 +1,10 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+# print(menu)
+menu_item = MenuItem
+print(menu_item)
+coffee_maker = CoffeeMaker
+money_machine = MoneyMachine()
