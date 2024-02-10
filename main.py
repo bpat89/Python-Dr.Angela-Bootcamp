41938647802ef88de4c6381b445a8aea2317e69f
Patrick <bilshanpatrick@gmail.com>
2024-02-10 18:57:13 +0400

    Intermediate - The Quiz Project & the Benefits of OOP: 156. Working with Attributes, Class Constructors and the __init__() Function

--- main.py
+++ main.py
@@ -1,23 +1,12 @@
-from menu import Menu, MenuItem
-from coffee_maker import CoffeeMaker
-from money_machine import MoneyMachine
+class User :
 
-money_machine = MoneyMachine()
-coffee_maker = CoffeeMaker()
-menu = Menu()
-is_on = True
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+        self.followers = 0
 
-# coffee_maker.report()
-# money_machine.report()
-while is_on:
-    options = menu.get_items()
-    choice = input(f"What would you like?({options}):")
-    if choice == "off":
-        is_on = False
-    elif choice == "report":
-        coffee_maker.report()
-        money_machine.report()
-    else:
-        drink = menu.find_drink(choice)
-        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
-            coffee_maker.make_coffee(drink)
+user_1 = User("001", "First User's Name")
+user_2 = User("002", "Second User's Name")
+
+
+print(user_1.followers)
