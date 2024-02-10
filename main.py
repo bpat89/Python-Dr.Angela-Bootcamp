c786a3ea0356879a420492f1b463106ebfb8809b
Patrick <bilshanpatrick@gmail.com>
2024-02-10 19:30:37 +0400

    Intermediate - The Quiz Project & the Benefits of OOP: Adding Methods to a Class

--- main.py
+++ main.py
@@ -4,9 +4,19 @@ class User :
         self.id = user_id
         self.username = username
         self.followers = 0
+        self.following = 0
+
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
 user_1 = User("001", "First User's Name")
 user_2 = User("002", "Second User's Name")
 
 
+user_1.follow(user_2)
 print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
