699b70222a7736fc0a63b011b60f11d38c829d82
Patrick <bilshanpatrick@gmail.com>
2024-09-18 09:54:39 +0400

    Intermediate - Turtle & the Graphic User Interface(GUI) - Turtle Challenge 1 - Draw a Square

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from turtle import Turtle, Screen
 
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("Red")
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
+tim = Turtle()
+# timmy_the_turtle.shape("turtle")
+# timmy_the_turtle.color("Red")
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.right(90)
 
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.left(90)
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.left(90)
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.left(90)
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.left(90)
 
-
-
+for _ in range (4):
+    tim.forward(100)
+    tim.left(90)
 
 
 
