a43aca76c6dfbd280bd69d304e90273c50836b88
Patrick <bilshanpatrick@gmail.com>
2024-09-18 16:50:02 +0400

    Intermediate - Turtle & the Graphic User Interface(GUI) - Turtle Challenge 4 - Generate a Random Walk

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ import random
 
 tim = t.Turtle()
 
-colors = ["red", "orange red", "green yellow", "gold", "medium spring green", "blue", "amber"]
-def draw_shape(num_sides):
-    angle = 360 / num_sides
-    for _ in range(num_sides):
-    # So we know that if we have 360 degrees divided by the number of sides, then we get the angle.
-        tim.forward(100)
-        tim.right(angle)
+colors = ["red", "orange red", "green yellow", "gold", "medium spring green", "blue", "goldenrod"]
+directions = [0, 90, 180, 270]
+tim.pensize(15)
+tim.speed("fastest")
 
-for shape_side_n in range (3, 11):
+for _ in range(200):
     tim.color(random.choice(colors))
-    draw_shape(shape_side_n)
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
