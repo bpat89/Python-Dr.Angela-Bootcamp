6547f9fb3df022a43feb415bc3096ec19cb152a2
Patrick <bilshanpatrick@gmail.com>
2024-09-18 15:57:45 +0400

    Intermediate - Turtle & the Graphic User Interface(GUI) - Turtle Challenge 3 - Drawing Different Shapes

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import turtle as t
+import random
 
 tim = t.Turtle()
 
-for _ in range (15):
-    tim.forward(10)
-    tim.penup()
-    tim.forward(10)
-    tim.pendown()
+colors = ["red", "orange red", "green yellow", "gold", "medium spring green", "blue", "amber"]
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+    # So we know that if we have 360 degrees divided by the number of sides, then we get the angle.
+        tim.forward(100)
+        tim.right(angle)
+
+for shape_side_n in range (3, 11):
+    tim.color(random.choice(colors))
+    draw_shape(shape_side_n)
