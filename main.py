9a3262f02da850b689de55e7fdefd3265f925ba9
Patrick <bilshanpatrick@gmail.com>
2024-10-12 20:26:37 +0400

    Intermediate - Instances, State and Higher Order Function - Challenge: Make an Etch-A-Sketch App

--- main.py
+++ main.py
@@ -5,9 +5,24 @@ screen = Screen()
 
 def move_forward():
     tim.forward(10)
+def move_backward():
+    tim.backward(10)
+def move_left():
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
+def move_right():
+    tim.right(10)
 
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
 
 screen.listen()
-screen.onkey(key="space", fun= move_forward)
-
+screen.onkey(move_forward, "w")
+screen.onkey(move_backward, "s")
+screen.onkey(move_left, "a")
+screen.onkey(move_right, "d")
+screen.onkey(clear, "c")
 screen.exitonclick()
